9c690403d58b1e40e1cd28c8594385fe5323870a
subhom11 <subhomkundu@gmail.com>
2019-09-18 23:11:30 +0530

    Updates

---
+++ precache-manifest.036ce0b59dee3014ae49f2f45160d2dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfc693b56a933d34625de6f97fe07fb",
+    "url": "/redux-learn/index.html"
+  },
+  {
+    "revision": "f5f835e3586b7a546059",
+    "url": "/redux-learn/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "94509dbc0053136ecc51",
+    "url": "/redux-learn/static/js/2.7392f623.chunk.js"
+  },
+  {
+    "revision": "f5f835e3586b7a546059",
+    "url": "/redux-learn/static/js/main.cd1cb5e0.chunk.js"
+  },
+  {
+    "revision": "abad6818519e19985cb3",
+    "url": "/redux-learn/static/js/runtime~main.42b76c16.js"
+  }
+]);
