96e90d53965bf0daedc0a26340b0cb343d77ea40
subhom11 <subhomkundu@gmail.com>
2019-09-18 17:09:45 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from './actions';
+import { decrement } from './actions';
+
+
+function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+  return (
+    <div className="App m-5">
+      <h1 className="text-dark">Redux Counter: {counter}</h1>
+      <button className="btn btn-primary mx-5" onClick={() => dispatch(increment(5))}>+</button>
+      <button className="btn btn-danger" onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged ? <h2>Sensitive information</h2> : ''}
+    </div>
+  );
+}
+
+export default App;
